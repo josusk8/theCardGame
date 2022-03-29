10448dc437be247d5768bcff0c769d3645872750
josusk8 <josusk8@gmail.com>
2022-03-29 09:23:14 +0100

    Initial commit

---
+++ src/Table.java
@@ -0,0 +1,10 @@
+
+/*To-Do list:
+* player1 wins game with 1 card: "2", "1"-> Player 1 wins to 0
+* player2 wins game with 1 card: "1", "2"-> Player 1 wins to 0
+* tie: "1", "1", -> "Tie"
+* player1 wins witch several cards
+* */
+
+public class Table {
+}
---
+++ src/TableShould.java
@@ -0,0 +1,2 @@
+public class TableShould {
+}
