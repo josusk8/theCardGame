b9f089afeba270a71bc5a5de164d9884b579728c
josusk8 <josusk8@gmail.com>
2022-03-29 09:59:19 +0100

    version 1

--- src/Table.java
+++ src/Table.java
@@ -1,10 +1,30 @@
 
 /*To-Do list:
-* player1 wins game with 1 card: "2", "1"-> Player 1 wins to 0
-* player2 wins game with 1 card: "1", "2"-> Player 1 wins to 0
-* tie: "1", "1", -> "Tie"
-* player1 wins witch several cards
-* */
+ * player1 wins game with 1 card: "2", "1"-> Player 1 wins to 0
+ * player2 wins game with 1 card: "1", "2"-> Player 2 wins to 0
+ * tie: "1", "1", -> "Tie"
+ * player1 wins witch several cards
+ * */
 
 public class Table {
+
+    String[] rank = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "k"};
+
+
+    static String game(int player1, int player2) {
+        String result = "";
+
+        if (player1 > player2) {
+            result = "player1 wins";
+        }
+        if (player1 < player2) {
+            result = "player2 wins";
+        }
+        if (player1 == player2) {
+            result = "Tie";
+        }
+
+        return result;
+    }
+
 }
--- src/TableShould.java
+++ src/TableShould.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TableShould {
+
+    @Test
+    void winPlayer1(){
+        int player1= 2;
+        int player2= 1;
+        assertEquals(Table.game(player1,player2),"player1 wins" );
+
+    }
+    @Test
+    void winPlayer2(){
+        int player1= 1;
+        int player2= 2;
+        assertEquals(Table.game(player1,player2),"player2 wins" );
+
+    }
+    @Test
+    void tie(){
+        int player1= 1;
+        int player2= 1;
+        assertEquals(Table.game(player1,player2),"Tie" );
+
+    }
+
+
 }
